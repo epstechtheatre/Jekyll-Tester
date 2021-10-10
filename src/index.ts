e67cfb94e74078260b0fc3e6d6cb17b1bc1b7196
Ben <1123bmac@gmail.com>
2021-10-09 17:44:07 -0700

    initial project commit

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import {spawn, exec} from "child_process";
+import fs from "fs";
+
+async function run() {
+    let path = core.getInput("path");
+    if (!path) path = "";
+    if (path.startsWith("/") || path.startsWith("\\")) {
+        path = path.substring(1); 
+    }
+
+    const working = `${process.cwd()}${path.length > 0 ? "/" + path : "" }`
+
+    if (!fs.existsSync(`${process.cwd()}/${path}`)) {
+        core.setFailed(`Path ${path} not found!`);
+        return; 
+    }
+
+    //Setup jekyll and ruby
+    const jekyllInstall = exec('gems install jekyll bundler', {
+        cwd: working
+    }, (err, stdout, stdin) => {
+        if (err) {
+            core.setFailed("Failed to install Jekyll, Bundler");
+            process.exit(1);
+        }
+    })
+    jekyllInstall.stdout?.pipe(process.stdout);
+
+    jekyllInstall.on("exit", () => {
+        const installGems = exec("bundle install", {
+            cwd: working
+        });
+
+        installGems.stdout?.pipe(process.stdout);
+        installGems.stderr?.pipe(process.stderr);
+
+        installGems.on("exit", () => {
+            let success = false;
+            function unexpectedExit() {
+                core.setFailed("Jekyll Failed to Launch")
+            }
+
+            const jekyllProcess = spawn(`bundle`, [`exec`, `jekyll`, `serve`], {
+                cwd: working
+            });
+            jekyllProcess.on("exit", unexpectedExit)
+            jekyllProcess.stderr.pipe(process.stderr);
+            jekyllProcess.stdout.on("data", (data: string) => {
+                if (data.toLowerCase().search("server running") !== -1 && !success) {
+                    jekyllProcess.off("exit", unexpectedExit);
+                    success = true;
+
+                    jekyllProcess.kill();
+                    core.setOutput("success", true);
+                }
+            })
+        })
+    })
+}
+
+run();
